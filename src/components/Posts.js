ab49219da82be46becb8db08e260aed4161bfb45
Patrick Mattei <patrick.mattei@gmail.com>
2020-09-28 17:07:14 -0400

    initial commit; posts displaying

---
+++ src/components/Posts.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default function Posts(props) {
+	const [posts, setPosts] = useState([]);
+	const getPosts = async () => {
+		try {
+			const response = await fetch("http://localhost:3000/posts");
+			const data = await response.json();
+			setPosts(data);
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	useEffect(() => {
+		(async function () {
+			await getPosts();
+		})();
+	}, []);
+
+	return (
+		<div>
+			<h2>Posts</h2>
+			{posts.map((post) => {
+				return (
+					<div key={post.id} className="post">
+						<h3>Date: {post.date}</h3>
+						<p>User ID:{post.user_id}</p>
+						<p>Entry: {post.entry}</p>
+						<hr />
+					</div>
+				);
+			})}
+		</div>
+	);
+}
