eecc7d5f34897216eeb09e483fa5dfabcb1d15fe
Patrick Mattei <patrick.mattei@gmail.com>
2020-09-29 18:06:00 -0400

    Access token being sent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import axios from "axios";
 
 import Posts from "./components/Posts.js";
 import Show from "./components/Show.js";
@@ -9,8 +10,51 @@ import Login from "./components/Login.js";
 import SignUp from "./components/SignUp.js";
 import Edit from "./components/Edit.js";
 
-export default function App (props) {
+export default function App() {
+	const history = useHistory();
+
 	const [posts, setPosts] = useState([]);
+	const [state, setState] = useState({
+		username: "",
+		password: "",
+	});
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const handleLogin = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await axios.post(
+				(process.env.REACT_APP_API_URL || "http://localhost:3000") + "/users/login",
+				{
+					user: {
+						username: state.username,
+						password: state.password,
+					}
+				}
+			);
+			localStorage.token = await response.data.token;
+			setIsLoggedIn(true);
+			setState({
+				username: "",
+				password: "",
+			});
+			//history.push("/");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+        if (localStorage.token) {
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    }, [isLoggedIn]);
+
+	const handleInput = (event) => {
+		setState({ ...state, [event.target.name]: event.target.value });
+	};
 
 	return (
 		<div className="App">
@@ -20,25 +64,20 @@ export default function App (props) {
 					<Switch>
 						<Route
 							path={`/users/login`}
-							component={Login}
+							render={() => {
+								return (
+									<Login
+										isLoggedIn={isLoggedIn}
+										handleInput={handleInput}
+										handleLogin={handleLogin}
+									/>
+								);
+							}}
 						/>
-						<Route
-							path={`/users/signup`}
-							component={SignUp}
-						/>
-						<Route
-							path={`/posts/new`}
-							component={New}
-						/>
-						<Route
-							path={`/posts/:id/edit`}
-							component={Edit}
-						/>
-						<Route
-							path={`/posts/:id`}
-							component={Show}
-							posts={posts}
-						/>
+						<Route path={`/users/signup`} component={SignUp} />
+						<Route path={`/posts/new`} component={New} />
+						<Route path={`/posts/:id/edit`} component={Edit} />
+						<Route path={`/posts/:id`} component={Show} posts={posts} />
 						<Route
 							path="/"
 							render={() => {
@@ -50,4 +89,4 @@ export default function App (props) {
 			</div>
 		</div>
 	);
-};
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Login (props) {
-	return <h1>Login Page</h1>
+	
+	return (
+		<form onSubmit={props.handleLogin}>
+            <h1>Log In</h1>
+			<label>Username</label>
+			<input type="text" name="username" onChange={props.handleInput} />
+
+
+			<label>Password</label>
+			<input type="password" name="password" onChange={props.handleInput} />
+
+			<input type="submit" className="submit" value="Login"/>
+        </form>
+	)
 }
